099fd7c162ae2368152a32c5c6cb47cfd474e37a
chriswburton <nuclearcornflakes@gmail.com>
2017-06-19 12:43:22 +0100

    Template initialised

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { NgShopPage } from './app.po';
+
+describe('ng-shop App', () => {
+  let page: NgShopPage;
+
+  beforeEach(() => {
+    page = new NgShopPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
